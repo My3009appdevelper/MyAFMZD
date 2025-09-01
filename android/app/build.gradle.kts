2909f975f1d1d4fc29c4f33025438c1636c275b6
Mau <maufuku3009@gmail.com>
2025-08-31 23:27:55 -0600

    ASIGNACIONES Y COLABORADORES

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.example.myafmzd"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23 // 🔧 Necesario actualizar
+        minSdk = flutter.minSdkVersion // 🔧 Necesario actualizar
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
